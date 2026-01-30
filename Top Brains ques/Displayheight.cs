b382fecf30b615373500dcee01b0b7145bff4d62
Aakarsh Dwivedi <117529682+aakarsh-d@users.noreply.github.com>
2026-01-30 11:55:49 +0530

    Add Displayheight.cs to categorize height

---
+++ Top Brains ques/Displayheight.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution
+{
+    public string GetHeightCategory(int heightCm)
+    {
+        if (heightCm < 150)
+            return "Short";
+        else if (heightCm < 180)
+            return "Average";
+        else
+            return "Tall";
+    }
+}
